b82f508978ab348bae010c3f2e6aee55c4ab8225
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-07-08 12:47:11 -0500

    Add: Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <header className="flex items-center h-16 bg-[#011627] text-white">
+      <div className="flex justify-between items-center w-full mx-6">
+        <Link to="/">
+          <h2 className="text-2xl font-medium">Landmarks</h2>
+        </Link>
+
+        <nav role="navigation">
+          <ul>
+            <li>
+              <Link
+                className="p-2 bg-[#FF3366] rounded-lg"
+                to="/landmarks/create"
+              >
+                Create
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,28 +1,10 @@
-import { Link, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 
 export default function AppLayout() {
   return (
     <>
-      <header className="flex items-center h-16 bg-[#011627] text-white">
-        <div className="flex justify-between items-center w-full mx-6">
-          <Link to="/">
-            <h2 className="text-2xl font-medium">Landmarks</h2>
-          </Link>
-
-          <nav role="navigation">
-            <ul>
-              <li>
-                <Link
-                  className="p-2 bg-[#FF3366] rounded-lg"
-                  to="/landmarks/create"
-                >
-                  Create
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
+      <Navbar />
 
       <main>
         <Outlet />
