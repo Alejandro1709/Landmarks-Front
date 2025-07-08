e90e9aebe4dfd803dbe0a493448dadd01d9c7182
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-07-08 12:45:51 -0500

    Add: AppLayout ui

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,3 +1,32 @@
+import { Link, Outlet } from 'react-router-dom'
+
 export default function AppLayout() {
-  return <div>AppLayout</div>
+  return (
+    <>
+      <header className="flex items-center h-16 bg-[#011627] text-white">
+        <div className="flex justify-between items-center w-full mx-6">
+          <Link to="/">
+            <h2 className="text-2xl font-medium">Landmarks</h2>
+          </Link>
+
+          <nav role="navigation">
+            <ul>
+              <li>
+                <Link
+                  className="p-2 bg-[#FF3366] rounded-lg"
+                  to="/landmarks/create"
+                >
+                  Create
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
 }
